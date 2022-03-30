a8117ef7bd1c50fdb7da5bcaba70af203e261012
Tri Waluyo <46350497+triwaluyo07@users.noreply.github.com>
2022-03-30 21:33:51 +0700

    MainActivityUpdate

--- app/src/main/java/com/example/hanacarakaproject/MainActivity.kt
+++ app/src/main/java/com/example/hanacarakaproject/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.hanacarakaproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hanacarakaproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        intentActivity()
+    }
+
+    private fun intentActivity()
+    {
+        binding.cardKamus.setOnClickListener {
+            val intent = Intent(this, KamusActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.cardSejarah.setOnClickListener {
+            val intent = Intent(this, SejarahActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
